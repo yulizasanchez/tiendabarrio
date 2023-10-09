9816deceddb33bdabdd7d0c1aebd28440c5633d4
yulizasanchez <yulizasanchez641@gmail.com>
2023-10-09 11:41:52 -0500

    inicio con saludos

--- Main.java
+++ Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String saludo = "welcome a mi tienda de barrio";
+        System.out.printf(saludo);
 
     }
 }
